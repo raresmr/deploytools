f050b892f2d7526c43ce37cdf359a24cc44eb571
raresmr <rares@visual.ly>
2016-06-22 18:32:49 +0300

    compare versions and update

--- fabfile.py
+++ fabfile.py
@@ -6,26 +6,49 @@
 from fabric.api import *
 
 
-env.hosts = [
-    '10.0.15.21',
-    '10.0.15.22'
-  # 'ip.add.rr.ess
-  # 'server2.domain.tld',
-]
+#env.hosts = [
+#    '10.0.15.21',
+#    '10.0.15.22'
+#]
 # Set the username
 env.user   = "vagrant"
 
 # Set the password [NOT RECOMMENDED]
 env.password = "vagrant"
-list=[]
-def update_npm():
-     npmv = run("npm -v")
-     list.append(npmv)
-     return max(list)
+ip_ver={}
+def web1_dev():
+    env.hosts = ['10.0.15.22']
+
+def web2_prod():
+    env.hosts = ['10.0.15.21']
+
+def all():
+    env.hosts = ['10.0.15.21', '10.0.15.22']
+
+def new_ver(max_ver):
+   run("echo %s > ~/max" % max_ver)
+   # sudo("npm install -g npm@%s" % max_ver)
 
 def npm_ver():
-    z = update_npm()
-    print 'asta' + z 
-    run("echo %s > ~/test" % z)
-    run("sudo npm install -g npm@%s" % z)
-	
+     npmv = run("npm -v")
+     ip_ver[env.host] = npmv
+     
+     if len(ip_ver) == len(env.hosts):
+        print 'ok'
+     return ip_ver
+
+def command():
+     dict_v = npm_ver()
+     print dict_v
+
+     max_ver = max(list(dict_v.values()))
+     print max_ver
+      
+     for key, value in dict_v.iteritems():
+       if value != max_ver:   
+        print 'install ' + key + ' ' + max_ver
+       # return max_ver
+        new_ver(max_ver)          
+       else:
+        print 'not ' + key + ' ' + value
+
